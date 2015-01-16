0c6b9deec0957c00a7b6826f80f2a88c77af15c6
Archit Srivastava <arch.srivastava.2@gmail.com>
2015-01-16 12:40:27 +0530

    [FEATURE] Added problem page for contests. To be accessed by contest/contest_code. Added relevant links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,19 +15,35 @@ class ApplicationController < ActionController::Base
 
   before_filter :authenticate_user!
 
-  def contests
+  def home
     @main_content_page = true
-    @contest_page = true
+    @home_page = true
     @title = "Contests"
-    @description = "See the oncoming, upcoming and past contests"
+    @description = "See the ongoing, upcoming and past contests"
     @clarifications = clarifications
-    @upcoming_contests = Contest.where({ start_time: { :$gt => DateTime.now } })
-    @ongoing_contests = Contest.where({ start_time: { :$lte => DateTime.now }, end_time: { :$gte => DateTime.now } })
-    @past_contests = Contest.where({ end_time: { :$lt => DateTime.now } })
+    upcoming_contests = Contest.where({ start_time: { :$gt => DateTime.now } })
+    ongoing_contests = Contest.where({ start_time: { :$lte => DateTime.now }, end_time: { :$gte => DateTime.now } })
+    past_contests = Contest.where({ end_time: { :$lt => DateTime.now } })
+    @contests_array = []
+    @contests_array << { data: upcoming_contests, name: "Upcoming Contests", panel_color: "info" }
+    @contests_array << { data: ongoing_contests, name: "Ongoing Contests", panel_color: "success" }
+    @contests_array << { data: past_contests, name: "Past Contests", panel_color: "danger" }
   end
 
   def clarifications
     return nil
+  end
+
+  def contests
+    contest_code = params[:ccode]
+    contest = Contest.where({ ccode: contest_code }).first
+    redirect_to controller: 'error', action: 'error_404' if contest.nil?
+    @main_content_page = true
+    @contest_page = true
+    @title = contest[:name]
+    @description = "Problems and rules for " + @title
+    problems = contest.problems.order_by({ submissions_count: -1 })
+    @problems_hash = { data: problems, name: "Problems for " + contest[:name], panel_color: "purple" }
   end
 
 end
