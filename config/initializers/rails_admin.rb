1810e50905b6924c909119a4ae9a24c3895acd9d
Archit Srivastava <arch.srivastava.2@gmail.com>
2015-01-16 14:38:09 +0530

    [FEATURE] Added HTTPS authentication for admin panel

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,12 @@
 RailsAdmin.config do |config|
 
+  config.authorize_with do
+    authenticate_or_request_with_http_basic('Login required') do |username, password|
+      username == Rails.application.secrets.user &&
+      password == Rails.application.secrets.password
+    end
+  end
+
   ### Popular gems integration
 
   ## == Devise ==
