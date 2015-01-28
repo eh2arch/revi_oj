c60e2570538fadb877b0b4248778e4331b176618
Exile101 <sigma.6.2009@gmail.com>
2015-01-28 14:03:17 +0530

    [FEATURE] Tables partly styled,submissions buttons added, timer(unstyled) added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
 
   ## Database authenticatable
   field :email,              type: String, default: ""
