fecbb8413a8808f82031ff0f807934be71c6fb55
Neetesh Kumar <kumarneetesh24@gmail.com>
2016-01-22 00:38:37 +0530

    [BUGFIX] rejudge reload problem fixed and added rejudge page feature

--- app/assets/javascripts/submissions.js
+++ app/assets/javascripts/submissions.js
@@ -36,29 +36,57 @@ jQuery(document).ready(function() {
                 }
 
                 element.setAttribute('data-status-code', submission_data['status_code']);
-                element.innerHTML = " <img src='/assets/" + img_attr + "' height='24' width='24' data-toggle='tooltip' data-placement='bottom' title='" + submission_data['status_code'] + "' data-original-title='" + status_description + "' > <br /><br /> <span><i><b>" + status_description + "</b></i></span>";
+                jQuery(element).children().remove();
+                jQuery(element).append(" <img src='/assets/" + img_attr + "' height='24' width='24' data-toggle='tooltip' data-placement='bottom' title='" + submission_data['status_code'] + "' data-original-title='" + status_description + "' > <br /><br /> <span><i><b>" + status_description + "</b></i></span>");
+                jQuery(element).siblings('.rejudge_submission').find('img').show('slow');
               }
+            },
+            error: function() {
+                setTimeout(function(){
+                    get_submission_data(element, submission_id);
+                }, 3000);                                
             },
             dataType: "json"
         });
     }
 
+    function rejudge_submission_single(elements) {
+        var img_attr = "load.gif";
+        for (var i=0; i < jQuery(elements).length; i++) {
+            var element = elements[i];
+            jQuery(element).children().remove();
+            jQuery(element).append(" <img src='/assets/" + img_attr + "' height='24' width='24' /> ");
+            var submission = jQuery(element).attr('data-submission-id');
+            rejudge_submission_call(element, submission);
+        }
+    }
     jQuery(".rejudge_submission").click(function(){
-        rejudge_submission(this.getAttribute('data-submission-id'));
-        img_attr = "load.gif";
-        this.innerHTML = " <img src='/assets/" + img_attr + "' height='24' width='24' /> ";
+        var submission = jQuery(this).attr('data-submission-id');
+        var element = jQuery("td.submission_img[data-submission-id="+submission+"]");
+        rejudge_submission_single(element);
     });
 
-    function rejudge_submission(submission_id) {
+     jQuery(".rejudge_page").click(function(){
+        var elements = jQuery("td.submission_img[data-submission-id]");
+        rejudge_submission_single(elements);
+    });
+    
+
+    function rejudge_submission_call(element, submission_id) {
+        jQuery(element).siblings('.rejudge_submission').find('img').hide('slow');
         jQuery.ajax(
         {
             url: "/rejudge",
             data: { "submission_id": submission_id },
-            dataType: "json"
+            type: "GET",
+            success: function(data)
+            {
+                get_submission_data(element, submission_id);
+            }
         });
     }
 
-    pending_elements = jQuery("td[data-status-code='PE'");
+    pending_elements = jQuery("td[data-status-code='PE']");
     for (i = 0; i < pending_elements.size(); i++) {
         get_submission_data(pending_elements[i], pending_elements[i].getAttribute('data-submission-id'));
     }
