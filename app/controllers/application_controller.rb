d43c01546408372921384d36952eacb10226d966
Archit Srivastava <arch.srivastava.2@gmail.com>
2016-01-28 21:03:30 +0530

    [BUGFIX] Fixed INvalidAuthenticityToken bug (devise) in sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  skip_before_filter :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'create' }
 
   before_action :authenticate_user!
 
